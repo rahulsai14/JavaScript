77b2fb8ec95127fe52dc575e67d35ce46c198f9b
rahulsai14 <98695626+rahulsai14@users.noreply.github.com>
2025-11-30 22:14:14 +0530

    Add files via upload
    Variables in JS

---
+++ Lesson_1/variables.js
@@ -0,0 +1,24 @@
+// Variables are containers for data, used to store data
+// undefined - Don't know what is there
+// null - We know what is there but the value is empty
+
+// JS is a dynamically typed language - in the run it takes the data type
+
+//Variable rules
+// Variable - Only can include letters, digits, underscore and $, Spaces not allowed
+//          - Should start with letter, underscore or $
+//          - No keywords should be used as varibale names
+
+// Different type of defining variables
+// var - variable can be re-declared and updated, A global scope variable
+// let - variable cannot be re-declared but can be updated, A block scope variable {}
+// const - variable cannot be re-declared or updated, A block scope variable {}
+
+// We use max let or const came in ES6
+
+let name = "Rahul Sai";
+
+console.log(name)
+
+// const - we should intialize with a value
+// let - we can declare without intialize the value
