2be6b4bc1b117f8e36c5a42096ae7378d4beec07
rahulsai14 <98695626+rahulsai14@users.noreply.github.com>
2025-11-30 22:20:34 +0530

    Create operators.js
    Operators

---
+++ Lesson_2/operators.js
@@ -0,0 +1,37 @@
+// Used to perform some operaion on data
+// Arithmetic Operators
+//  +, -, *, /, %, Exponentiation
+
+let a = 5;
+let b = 6;
+let c = a + b;
+console.log("The sum is: ", c);
+
+// ** -> Exponentiation 5^2 = 5 ** 2
+
+// Unary operators
+// Increment ++,  Decrement --
+
+// a++ - post increment (Value will change after)
+// ++a - pre increment (Value will change before)
+
+console.log(++a); // It will print 6
+console.log(a++); // It will print 5
+
+// Assignment Operators
+// To assign a value to variables
+// =, +=, -=, *=, %=, **=, /=
+
+// Comparison operators
+// ==, !=, >, >=, <, <=
+// Output will be either true or false
+// Let's say when compare a number and number in string like "5" 
+// Then first JS converts that string to number and then comparies it
+
+// === - Strict version (Checks even the data type of both sides)
+// !== - Strict version
+
+
+// Logical operators
+// &&, ||, !
+// Output will be either true or false
