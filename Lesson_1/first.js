53f13ec4b455fc343990294584f818b184f37c91
rahulsai14 <98695626+rahulsai14@users.noreply.github.com>
2025-11-30 22:11:53 +0530

    Create first.js

---
+++ Lesson_1/first.js
@@ -0,0 +1,8 @@
+// We use programming languages to give instructions to computer
+// The simple way to write and JS is we can run in chrome browser
+// In Inspect - Consolel
+
+// To print a message to console, log(print)l
+console.log("Hello, Rahul!");
+
+// To connect JS code written here to Browser, we use a HTML file
